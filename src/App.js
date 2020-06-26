71834954a1277f372e2bf0b6bcd0716eb59bbb7e
ablakerose <bdecktor@gmail.com>
2020-06-26 15:01:03 -0400

    add page components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 import "./App.css";
+import Main from "./components/main";
 
 function App() {
   return (
@@ -8,22 +9,23 @@ function App() {
       <Layout>
         <Header title="Title" scroll>
           <Navigation>
-            <a href="#">Link</a>
-            <a href="#">Link</a>
-            <a href="#">Link</a>
-            <a href="#">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
           </Navigation>
         </Header>
         <Drawer title="Title">
           <Navigation>
-            <a href="#">Link</a>
-            <a href="#">Link</a>
-            <a href="#">Link</a>
-            <a href="#">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
           </Navigation>
         </Drawer>
         <Content>
           <div className="page-content" />
+          <Main />
         </Content>
       </Layout>
     </div>
