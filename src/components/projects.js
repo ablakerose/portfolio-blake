cc8a24ebf13a41803ca759cbeb26da52b0617a16
ablakerose <bdecktor@gmail.com>
2020-06-30 14:38:32 -0400

    add tab framework to projects page

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
+//import { Tab } from
 
 class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activateTab: 0 };
+  }
+
   render() {
     return (
-      <div>
+      <div className="category-tabs">
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={(tabId) => this.setState({ activeTab: tabId })}
+          ripple
+        ></Tabs>
         <h1>Projects</h1>
       </div>
     );
